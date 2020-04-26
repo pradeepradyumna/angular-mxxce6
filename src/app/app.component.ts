37110e6130be4a7a475efe95d74ab40d647aa5ea
pradeepradyumna <pradeepradyumna@gmail.com>
2020-04-26 16:49:33 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Todo } from "./todo";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
-  
+export class AppComponent {
+  name = "Angular";
+
+  todoValue: string;
+  list: Todo[];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem() {
+    if (this.todoValue !== "") {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = "";
+  }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter(i => i.id !== id);
+  }
 }
